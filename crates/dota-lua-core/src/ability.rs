996ee41e3348a1baa99b3911763a715ee8098297
r41ngee <r41ngee@yandex.ru>
2026-07-21 19:42:40 +0300

    fix: add missing types import and unused attribute for Ability trait

--- crates/dota-lua-core/src/ability.rs
+++ crates/dota-lua-core/src/ability.rs
@@ -1,7 +1,9 @@
 use crate::enums;
+use crate::types;
 
 #[allow(non_snake_case)]
+#[allow(unused)]
 pub trait Ability {
     fn CastFilterResult() -> enums::unit_filter::UnitFilterResult { enums::unit_filter::UnitFilterResult::UF_SUCCESS }
-    // fn 
+    fn CastFilterResultLocation(location: types::vector::Vector) -> enums::unit_filter::UnitFilterResult { enums::unit_filter::UnitFilterResult::UF_SUCCESS }
 }
