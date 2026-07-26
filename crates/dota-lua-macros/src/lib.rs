9b62610ab6e8d9edeb015f3c5c9c798bb37e5716
r41ngee <r41ngee@yandex.ru>
2026-07-27 01:09:07 +0300

    feat: add ability macro for attribute processing in dota-lua

--- crates/dota-lua-macros/src/lib.rs
+++ crates/dota-lua-macros/src/lib.rs
@@ -4,3 +4,8 @@ use proc_macro::TokenStream;
 pub fn projectile_data(_: TokenStream) -> TokenStream {
     TokenStream::new()
 }
+
+#[proc_macro_attribute]
+pub fn ability(_: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
